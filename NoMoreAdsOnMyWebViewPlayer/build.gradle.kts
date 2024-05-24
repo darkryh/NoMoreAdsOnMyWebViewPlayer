a71e4b832d60f82ad8ea0de8c1f3bd26c341882f
darkryh_ <alex_torres-xc@hotmail.com>
2024-05-24 16:49:23 -0500

    updated publish

--- NoMoreAdsOnMyWebViewPlayer/build.gradle.kts
+++ NoMoreAdsOnMyWebViewPlayer/build.gradle.kts
@@ -42,7 +42,7 @@ publishing {
 
             groupId = "com.ead.lib"
             artifactId = "NoMoreAdsOnMyWebViewPlayer"
-            version = "0.0.1"
+            version = "0.0.2"
         }
     }
 }
