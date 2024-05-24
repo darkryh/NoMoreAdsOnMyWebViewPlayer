7b6087a97b5954404e5b223d5bcc8441bca35be1
darkryh_ <alex_torres-xc@hotmail.com>
2024-05-24 14:11:56 -0500

    example library

--- app/src/main/java/com/ead/app/nomoreadsonmywebviewplayer/MainActivity.kt
+++ app/src/main/java/com/ead/app/nomoreadsonmywebviewplayer/MainActivity.kt
@@ -4,18 +4,24 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.viewinterop.AndroidView
-import com.ead.app.nomoreadsonmywebviewplayer.ui.theme.NoMoreAdsOnMyWebViewPlayerTheme
+import com.ead.app.nomoreadsonmywebviewplayer.presentation.main.MainEvent
+import com.ead.app.nomoreadsonmywebviewplayer.presentation.main.MainViewModel
+import com.ead.app.nomoreadsonmywebviewplayer.presentation.theme.NoMoreAdsOnMyWebViewPlayerTheme
 import com.ead.lib.nomoreadsonmywebviewplayer.NoMoreAdsWebView
 import com.ead.lib.nomoreadsonmywebviewplayer.core.Blocker
 
 class MainActivity : ComponentActivity() {
+
+    private val viewModel : MainViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         Blocker.init(this)
@@ -23,9 +29,18 @@ class MainActivity : ComponentActivity() {
         setContent {
             NoMoreAdsOnMyWebViewPlayerTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    TestWebView(
-                        modifier = Modifier.padding(innerPadding)
+                    NoMoreAdsWebView(
+                        modifier = Modifier.padding(innerPadding),
+                        event = viewModel::onEvent
                     )
+
+                    LaunchedEffect(key1 = true) {
+                        viewModel.onEvent(
+                            MainEvent.LoadingUrl(
+                                url = "https://uqload.to/embed-b0airhfed337.html"
+                            )
+                        )
+                    }
                 }
             }
         }
@@ -33,12 +48,12 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun TestWebView(modifier: Modifier = Modifier) {
+fun NoMoreAdsWebView(modifier: Modifier = Modifier, event: (MainEvent) -> Unit) {
     AndroidView(
         modifier = modifier.fillMaxSize(),
         factory = { context ->
             NoMoreAdsWebView(context).apply {
-                loadUrl("https://asnwish.com/e/ktqgf3vcaniy")
+                event(MainEvent.InitializeWebView(this))
             }
         }
     )
@@ -49,6 +64,8 @@ fun TestWebView(modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     NoMoreAdsOnMyWebViewPlayerTheme {
-        TestWebView()
+        NoMoreAdsWebView(
+            event = {}
+        )
     }
 }
