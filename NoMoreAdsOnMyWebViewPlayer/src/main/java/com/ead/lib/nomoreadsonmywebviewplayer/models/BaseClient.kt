e71e72528e5491d239c239e8f5369fec5c5c2dda
darkryh <alex_torres-xc@hotmail.com>
2024-09-24 12:55:23 -0500

    open onPassingOverrideUrl and onPassingInterceptRequest  functions to work with testing, and documented code

--- NoMoreAdsOnMyWebViewPlayer/src/main/java/com/ead/lib/nomoreadsonmywebviewplayer/models/BaseClient.kt
+++ NoMoreAdsOnMyWebViewPlayer/src/main/java/com/ead/lib/nomoreadsonmywebviewplayer/models/BaseClient.kt
@@ -4,6 +4,7 @@ import android.webkit.WebResourceRequest
 import android.webkit.WebResourceResponse
 import android.webkit.WebView
 import android.webkit.WebViewClient
+import androidx.annotation.VisibleForTesting
 import java.io.ByteArrayInputStream
 import java.net.MalformedURLException
 import java.net.URL
@@ -16,13 +17,17 @@ open class BaseClient(
     /**
      * @param *url to validate is called when a url is loading
      */
-    var url : String) :  WebViewClient() {
+    var url : String
+    /**
+     * Inherited class from WebViewClient
+     */
+) :  WebViewClient() {
 
 
     /**
      * Empty resource for the web view to block the request
      */
-    protected val emptyResource : WebResourceResponse =
+    internal val emptyResource : WebResourceResponse =
         WebResourceResponse(
             "text/plain",
             "utf-8",
@@ -35,33 +40,53 @@ open class BaseClient(
     /**
      * @return domain of the url to intercept and validate the loading
      */
-    private val domain : String? get() = run {
+    @VisibleForTesting(otherwise = VisibleForTesting.PRIVATE)
+    private val domain: String? get() = run {
 
+        /**
+         * Getting the url to validated isn't malformed
+         */
+        val url = try { URL(this@BaseClient.url) } catch (e: MalformedURLException) { null }
 
 
         /**
-         * check the structure of the url
+         * Getting the host of the url by splitting the domain
          */
-        val url = try { URL(this.url) } catch (e: MalformedURLException) { null }
-
+        val hostParts = url?.host?.split('.')
 
 
         /**
-         * and finally return de domain
+         * if the host parts are more than 2 return the second last part, that is consider the domain
          */
-        url?.host?.split('.')?.firstOrNull { it != "www" } ?: url?.host
+        return if (hostParts != null && hostParts.size >= 2) {
+            hostParts[hostParts.size - 2]
+        } else {
+
+            /**
+             * if the host parts are less than 2 return the host
+             */
+            url?.host
+        }
     }
+
 
 
 
     /**
-     * @return regex to validate the url
+     * @return regex to validate the url by the domain in the loading url
      */
-    private val permittedRegex : Regex? get() = run {
+    private val permittedRegexByDomain : Regex? get() = run {
         val domain = domain ?: return null
         ".*${domain}.*".toRegex()
     }
 
+
+    /**
+     * @return regex to validate the url of embed sites
+     */
+    private val permittedRegexes : List<Regex> = listOf(
+        """https://[a-zA-Z0-9.-]+/(e|embed|watch|player)/[a-zA-Z0-9]+""".toRegex()
+    )
 
 
     /**
@@ -69,12 +94,12 @@ open class BaseClient(
      * in basic way just checking by domain
      * in case its passes the regex
      * call onPassingOverrideUrl
-     * to check the next layer of security
+     * to check the next layer of security [onPassingInterceptRequest]
      */
-    @Deprecated("use OnPassingOverrideUrl Instead", ReplaceWith("false"))
+    @Deprecated("use OnPassingOverrideUrl Instead", ReplaceWith("OnPassingOverrideUrl(view, request)"))
     override fun shouldOverrideUrlLoading(view: WebView?, request: WebResourceRequest?): Boolean {
-        return if (permittedRegex?.matches(request?.url.toString()) == true)
-            true
+        return if (onPassingFirstLayerOfSecurity(request,true))
+            false
         else
             onPassingOverrideUrl(view, request)
     }
@@ -86,9 +111,9 @@ open class BaseClient(
      * in basic way just checking by domain
      * in case its passes the regex
      * call onPassingInterceptRequest
-     * to check the next layer of security
+     * to check the next layer of security [onPassingInterceptRequest]
      */
-    @Deprecated("use onPassingInterceptRequest Instead", ReplaceWith("false"))
+    @Deprecated("use onPassingInterceptRequest Instead", ReplaceWith("onPassingInterceptRequest(view, request)"))
     override fun shouldInterceptRequest(
         view: WebView?,
         request: WebResourceRequest?
@@ -98,23 +123,31 @@ open class BaseClient(
         /**
          * check the structure of the url using the regex
          */
-        return if (permittedRegex?.matches(request?.url.toString()) == true)
+        return if (onPassingFirstLayerOfSecurity(request))
             super.shouldInterceptRequest(view, request)
         else
             onPassingInterceptRequest(view, request)
     }
 
 
+    protected fun onPassingFirstLayerOfSecurity(request: WebResourceRequest?, isOverriding : Boolean = false) : Boolean {
+        return permittedRegexByDomain?.matches(request?.url.toString()) == true ||
+                permittedRegexes.any {
+                    val urlMatchToRedirect = it.matches(request?.url.toString())
+                    if (isOverriding && urlMatchToRedirect) url = request?.url.toString()
+                    urlMatchToRedirect
+                }
+    }
 
     /**
      * Used to validate the overriding of the url on the next layer of security
      */
-    protected open fun onPassingOverrideUrl(view: WebView?, request: WebResourceRequest?) : Boolean = true
+    open fun onPassingOverrideUrl(view: WebView?, request: WebResourceRequest?) : Boolean = true
 
 
 
     /**
      * Used to validate the intercepting of the url on the next layer of security
      */
-    protected open fun onPassingInterceptRequest(view: WebView?, request: WebResourceRequest?) : WebResourceResponse? = null
+    open fun onPassingInterceptRequest(view: WebView?, request: WebResourceRequest?) : WebResourceResponse? = null
 }
